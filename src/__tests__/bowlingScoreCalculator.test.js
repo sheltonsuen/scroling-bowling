115c0c55cdd82a69fd07e57cab70684eafd7ff36
Shelton Suen <sheltonsuen@gmail.com>
2020-06-03 00:10:34 +0800

    feat - given 1st round X with following 1st ball no balls down and 2nd ball 1 ball down, rest no balls down when calculate total score then return 12 scores

--- src/__tests__/bowlingScoreCalculator.test.js
+++ src/__tests__/bowlingScoreCalculator.test.js
@@ -4,9 +4,28 @@ test('given 1st round X with following 2 balls no balls down, rest no balls down
     const scoreBoard = [{
         label: 'X',
         firstBallDown: 10
+    }, {
+        label: '',
+        firstBallDown: 0,
+        secondBallDown: 0
     }];
 
     const totalScore = calculateTotalScore(scoreBoard);
 
     expect(totalScore).toBe(10)
+});
+
+test('given 1st round X with following 1st ball no balls down and 2nd ball 1 ball down, rest no balls down when calculate total score then return 12 scores', () => {
+    const scoreBoard = [{
+        label: 'X',
+        firstBallDown: 10
+    }, {
+        label: '',
+        firstBallDown: 0,
+        secondBallDown: 1,
+    }];
+
+    const totalScore = calculateTotalScore(scoreBoard);
+
+    expect(totalScore).toBe(12)
 });
--- src/bowlingScoreCalculator.js
+++ src/bowlingScoreCalculator.js
@@ -1,3 +1,6 @@
-export const calculateTotalScore = () => {
-    return 10;
+export const calculateTotalScore = (scoreBoard) => {
+    if (scoreBoard[0].label === 'X') {
+
+        return scoreBoard[0].firstBallDown + (scoreBoard[1].firstBallDown + scoreBoard[1].secondBallDown) * 2
+    }
 };
